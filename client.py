cfc40e662a63489ee50dc0421aab082ba2c627c2
MosheNacu <42210514+MosheNacu@users.noreply.github.com>
2019-11-06 17:47:35 +0800

    not working yet

--- client.py
+++ client.py
@@ -1,11 +1,25 @@
 import socket
-TCP_IP = str(input("Specify Host IP Address: "))
-TCP_PORT = int(input("Specify Port: "))
+# TCP_IP = str(input("Specify Host IP Address: "))
+# TCP_PORT = int(input("Specify Port: "))
+TCP_IP = "127.0.0.3"
+TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = b"Hello, World!"
+#=============JOIN=================
+name = input("Enter player name: ")
+MESSAGE = "JG:"+name
+
+#==================================
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.sendall(MESSAGE)
+s.send(MESSAGE.encode('utf8'))
 data = s.recv(BUFFER_SIZE)
+msg = str(data.decode('utf8'))
+if (msg == "AC"):
+	while True:
+		print("Waiting for other players")
+		print("[1] Vote Start")
+		print("[2] Leave Room")
+		x = input()
 s.close()
 print ("received data:", data)
+
--- server.py
+++ server.py
@@ -1,16 +1,36 @@
 import socket
-TCP_IP = '127.0.0.1'
+
+class Player:
+	def __init__(self, conn, addr, playerName):
+		self.conn = conn
+		self.addr = addr
+		self.playerName = playerName
+		self.vote = False
+
+TCP_IP = '127.0.0.3'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
+print("server is up and running ...")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
-conn, addr = s.accept()
-print ('Connection address:', addr)
-while True:
-  data = conn.recv(BUFFER_SIZE)
-  if not data: 
-    break
-  print ("received data:", data)
-  conn.send(data)  # echo
+
+players = []	#key: player name   value:conn
+
+while True: 							#server loop
+	while True: 						#invite loop
+		conn, addr = s.accept()
+		data = conn.recv(BUFFER_SIZE)
+		msg = str(data.decode('utf8'))
+		if msg[0:2] == "JG": 			#checks if player is joining game
+			if(len(players) < 13):
+				players.append(Player(conn, addr, msg[3:])) 	#adds player
+				print(players[0].playerName)
+				conn.send("AC".encode('utf8'))
+			else:
+				conn.send("RJ".encode('utf8'))
+		if msg[0:2] == "VT":
+
+
 conn.close()
+
